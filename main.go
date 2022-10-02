03b3db7ebdac9c0423ee42c35af3f6e17874f2bb
diegolopez-ing <diegolopez.coder@gmail.com>
2022-10-02 17:34:47 -0500

    Creacion de tablas atraves de migraciones

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"net/http"
 
 	"github.com/DiegoLopez-ing/api-gorm-mux/db"
+	"github.com/DiegoLopez-ing/api-gorm-mux/models"
 	"github.com/DiegoLopez-ing/api-gorm-mux/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	db.DBConection()
+	db.DB.AutoMigrate(models.Tasks{})
+	db.DB.AutoMigrate(models.User{})
 	r := mux.NewRouter()
 	r.HandleFunc("/", routes.HomeHandler)
 
