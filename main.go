3175859878cd77debf35a5652ea6f8b3847b8cdc
diegolopez-ing <diegolopez.coder@gmail.com>
2022-10-02 17:23:42 -0500

    Conexion a BD postgres

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"github.com/DiegoLopez-ing/api-gorm-mux/db"
 	"github.com/DiegoLopez-ing/api-gorm-mux/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	db.DBConection()
 	r := mux.NewRouter()
 	r.HandleFunc("/", routes.HomeHandler)
 
